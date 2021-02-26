7ee36428a9f56c9c329cc6f6a7f84e6080226c58
fathan <fathanmubina@upi.edu>
2021-02-26 09:12:47 +0700

    Aktifitas 1 Fathan

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("masukkan nama anda : ")
+umur=19
+
+#OUTPUT
+print("")
+print("===INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN===")
+print("nama Anda: ", nama)
+print("umur Anda: ", umur)
+print("")
